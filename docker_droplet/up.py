146437547f05c1e19db57e8f9566f705d39e611f
JoelLefkowitz <joellefkowitz@hotmail.com>
2020-02-08 19:22:15 +0000

    Add permission modifier for dynamic inventory script

--- docker_droplet/up.py
+++ docker_droplet/up.py
@@ -1,5 +1,6 @@
 import pathlib
-from os.path import join, dirname
+from stat import S_IEXEC
+from os.path import join, dirname, stat, chmod
 from subprocess import run
 
 from docker_droplet.terraform.template import create_config
@@ -7,14 +8,23 @@ from docker_droplet.terraform.template import create_config
 
 def set_up(droplet_name, ssh_key, token, project, domain, config_path):
 
+    # Create config from template
     with open(config_path, "w") as config_file:
         config_text = create_config(droplet_name, ssh_key, project, domain)
         config_file.write(config_text)
 
+    # Run terraform from config directory
     run(["terraform", "init"], cwd=dirname(config_path))
     run(["terraform", "apply"], cwd=dirname(config_path))
 
+    # Define ansible paths
     directory = pathlib.Path(__file__).parent.absolute()
     INVENTORY = join(directory, "ansible/inventory")
+    INVENTORY_SCRIPT = join(INVENTORY, 'digitalocean.py')
     PLAYBOOK = join(directory, "ansible/playbook.yml")
+
+    # Make sure dynamic inventory script is executable
+    chmod(INVENTORY_SCRIPT, stat(INVENTORY_SCRIPT).st_mode | S_IEXEC)
+
+    # Provision the droplet
     run(["ansible-playbook", "-i", INVENTORY, PLAYBOOK, "-u", "root"])
