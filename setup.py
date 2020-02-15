e293456b69b0e3683b86fa5319e34de667187226
JoelLefkowitz <joellefkowitz@hotmail.com>
2020-02-15 13:42:15 +0000

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup, Command
 from unittest import TestLoader, TextTestRunner
 from sphinx.setup_command import BuildDoc
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 
 class DocsCommand(BuildDoc):
